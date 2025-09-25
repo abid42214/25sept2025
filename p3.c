5025c6d70ae6336c5f0171b45ca209bf469e23ba
abid42214 <abidhossan566@gmail.com>
2025-09-25 12:49:38 +0600

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,16 @@
+//1. Positive, Negative or Zero
+//Problem: Write a program to check whether a number is positive, negative, or zero
+#include<stdio.h>
+int main (){
+    int num=0;
+if(num>0){ printf("IT is a positive number \n") ;}
+    if(num==0){ printf("IT is zero \n");}
+
+ else{
+    printf("IT is a negative number \n");
+}
+
+
+
+return 0;
+}
